3274f80010642847d737bd5d7808f2e6c5f5d57f
Scott Zager <scott.zager@gmail.com>
2012-09-21 22:26:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HockeySwap::Application.config.secret_token = '5561fece528aaa92c5b05e9388dc58149743e9c21082da92ed4f8f0ca1f68dcdbeaf12d67bd61ce59484d391f765008a252c19d5fe6a70402e4b85ebed71e6bf'
